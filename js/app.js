9070a27cf209c39328bd89724d89b6be58631787
Dillon Donoho <donoho.dillon@gmail.com>
2023-03-17 09:58:29 -0400

    j

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 async function setup() {
-    const patchExportURL = "export/patch.export.json";
+    const patchExportURL = "export/patch.export2.json";
 
     // Create AudioContext
     const WAContext = window.AudioContext || window.webkitAudioContext;
