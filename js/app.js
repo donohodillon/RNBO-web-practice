f41d03a69147b4cd51c3fd54cff5f05ad17c1cc2
Dillon Donoho <donoho.dillon@gmail.com>
2023-03-23 20:15:25 -0400

    "ok definitely fixed for mac"

--- js/app.js
+++ js/app.js
@@ -15,17 +15,18 @@ let sketch = function(p) {
         console.log("p5js setup working")
         p.createCanvas(400, 400);
     }
-  
+   
     p.draw = function() { 
-      p.background(0);
-      p.ellipse(200, 200, 50, 50);
+      p.background(255);
       console.log("Slider 0 value:", sliders[0].value());
-    console.log("Slider 1 value:", sliders[1].value());
+      console.log("Slider 1 value:", sliders[1].value());
 
       let sliderVal = sliders[0].value();
       let sliderVal1 = sliders[1].value();
     //   console.log("slider valueeee", sliders[0].value) 
       p.ellipse(200, 200, sliderVal * .75, sliderVal * .75);
+      p.ellipse(200, 200, sliderVal1 * .75, sliderVal1 * .75);
+      
     } 
   }
 
