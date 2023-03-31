b0f3b5bfcd4179a34d9b58d83b332f4247712477
Dillon Donoho <donoho.dillon@gmail.com>
2023-03-31 11:10:03 -0400

    Added another sketch

---
+++ js/sketch3.js
@@ -0,0 +1,41 @@
+let device;
+let numberOfDeviceParameters;
+let sliders = [];
+let sliderValue;
+
+let f = 0;
+
+async function setup() {
+
+  await RNBOsetup('export/patch.exportSAW.json');
+  makeP5jsSliders(); 
+
+  createCanvas(400, 400, WEBGL);
+	noStroke();
+}
+ 
+function draw() {
+
+  //IMPORTANT: Make sure you're checking that the sliders array is full before you use it
+
+  if (sliders.length > 0) { 
+  sliderValue = sliders[0].value();  
+  [3, 3, -3].map(i => pointLight([255], 0, -400 * i, 0));
+	let e = 180;
+	rotateY((f+(sliderValue*2)) / e);
+	for (y = -e; y <= e; y += 30) {
+		for (z = -e; z <= e; z += 30) {
+			for (x = -e; x <= e; x += 30) {
+				let Y = y - f % 30;
+				let E = e - dist(0, 0, 0, x, Y, z);
+				push();
+				translate(x, Y, z);
+				if (E > 0) sphere(E / 4, 33);
+				pop();
+			}
+		}
+	}
+	box(2000);
+	f++;
+  }
+}
